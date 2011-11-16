e491ffe351f40f7d19b5379c8c1138662324b496
Dexter Deng <dexter@gmail.com>
2011-11-17 03:34:03 +0800

    db

--- db/migrate/20111116133238_create_articles.rb
+++ db/migrate/20111116133238_create_articles.rb
@@ -4,7 +4,10 @@ class CreateArticles < ActiveRecord::Migration
       t.string       :name
       t.string       :description
       t.text         :content
+      t.boolean      :published,    :default => false
+      t.references   :user
       t.timestamps
     end
+
   end
 end
