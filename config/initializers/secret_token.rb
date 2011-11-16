b944393283e99121bd6e2aa361f1dcd150efdd76
Dexter Deng <dexter@gmail.com>
2011-11-16 11:15:29 +0800

    confir files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mwiki::Application.config.secret_token = '67575c609c43564dc9226706a27009a82c425527565d042285a102b28fc76d12679dce641bee82384edb7a30ea02a656e1b066c644e71789bac5b555c1953807'
